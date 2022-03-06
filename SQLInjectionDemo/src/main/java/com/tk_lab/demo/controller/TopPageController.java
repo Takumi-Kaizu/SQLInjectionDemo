a9b413300c8a7b432e1027dfb07da52ffa7e1097
mii15 <mii15@LAPTOP-3KJ2P2Q0>
2022-03-06 15:30:23 +0900

    トップページの実装を行った(必要な項目については後で加筆する)

--- SQLInjectionDemo/src/main/java/com/tk_lab/demo/controller/TopPageController.java
+++ SQLInjectionDemo/src/main/java/com/tk_lab/demo/controller/TopPageController.java
@@ -1,18 +1,13 @@
 package com.tk_lab.demo.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * トップページ用のコントローラー
  */
 @Controller
 public class TopPageController {
@@ -20,20 +15,16 @@ public class TopPageController {
     private static final Logger logger = LoggerFactory.getLogger(TopPageController.class);
 
     /**
-     * Simply selects the home view to render by returning its name.
+     * トップページを開く時のメソッド。
+     * 備忘録
+     * 1.コントローラー（ここ）で受け付け
+     * 2.受け付けたコントローラーメソッドで処理（モデル（サービスクラス、DAO クラス））
+     * 3.処理結果をビューへ渡す。
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}.", locale);
+    @RequestMapping(value = "/top", method = RequestMethod.GET)
+    public String home() {
 
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate);
-
-        return "home";
+        return "top";
     }
 
 }
